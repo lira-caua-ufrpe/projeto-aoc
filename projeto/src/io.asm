5d5229edf5090156cf37a852b01bca84b6e45447
Cauã UFRPE <caua.lira@ufrpe.br>
2025-10-29 11:51:48 -0300

    Create io.asm

---
+++ projeto/src/io.asm
@@ -0,0 +1,6 @@
+# ============================================================
+# Arquivo: io.asm
+# Descrição: Utilidades de I/O do projeto (a completar).
+# ============================================================
+.text
+# (adicione read_int, print_str etc. quando implementar)
