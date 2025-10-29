ab87305930f24930918aad6751ee124b0a9577d8
Cauã UFRPE <caua.lira@ufrpe.br>
2025-10-29 12:08:16 -0300

    Update io.asm

--- projeto/src/io.asm
+++ projeto/src/io.asm
@@ -1,6 +1,23 @@
 # ============================================================
-# Arquivo: io.asm
-# Descrição: Utilidades de I/O do projeto (a completar).
+# io.asm – wrappers simples de I/O (terminal padrão)
 # ============================================================
 .text
-# (adicione read_int, print_str etc. quando implementar)
+.globl print_str, print_int, read_line
+
+# print_str(a0=addr)
+print_str:
+    li $v0,4
+    syscall
+    jr $ra
+
+# print_int(a0=int)
+print_int:
+    li $v0,1
+    syscall
+    jr $ra
+
+# read_line(a0=buffer, a1=maxlen) -> lê até '\n' (usa syscall 8)
+read_line:
+    li $v0,8
+    syscall
+    jr $ra
