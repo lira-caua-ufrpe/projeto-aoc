eed7611f4d0195fd23db48e44b8f83996c519e8e
Cauã UFRPE <caua.lira@ufrpe.br>
2025-10-29 12:11:28 -0300

    Create fileio.asm

---
+++ projeto/src/fileio.asm
@@ -0,0 +1,9 @@
+# ============================================================
+# fileio.asm – salvar/recarregar usando syscalls 13..16
+# ============================================================
+.text
+.globl salvar_cmd, recarregar_cmd, formatar_cmd
+
+salvar_cmd:       jr $ra
+recarregar_cmd:   jr $ra
+formatar_cmd:     jr $ra
